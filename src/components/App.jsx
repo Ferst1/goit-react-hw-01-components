11b9fb5c921cf61249d456371a3a94d0cb7f02de
Elena <36508011+Ferst1@users.noreply.github.com>
2023-10-10 16:00:36 +0200

    transaction
    transaction history tabelle

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,13 @@
 import { Profile } from './Profile/Profile';
 import { Statistics } from './Statistic/Statistic';
 import { FriendList } from './FriendList/FriendList';
+import { TransactionHistory } from './HistoryTransactions/HistoryTransactions';
+
 
 import user from 'JSON/user.json';
 import data from 'JSON/data.json';
 import friends from 'JSON/friends.json';
+import transactions from 'JSON/transactions.json';
 
 
 export const App = () => {
@@ -20,6 +23,8 @@ export const App = () => {
 
 <Statistics title="Upload stats" stats={data} />
 <FriendList friends={friends} />
+<TransactionHistory items={transactions} />
+
     
     </div>
  
